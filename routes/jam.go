a2d5b55b7031388d127b66fe81638a52735a553d
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-11-18 20:30:08 -0800

    final release candidate

--- routes/jam.go
+++ routes/jam.go
@@ -16,7 +16,6 @@ type JamRouter struct {
 
 const (
 	jamR        = APIV + "jam"
-	jamByID     = jamR + "/:id"
 	recordingsR = jamR + "/recording/:id"
 	jamNewR     = jamR + "/new"
 	joinR       = jamR + "/join"
@@ -32,7 +31,7 @@ func NewJamRouter() JamRouter {
 // addToMainROuter func, will add all the jam routes
 // to he main router
 func (j *JamRouter) addToMainRouter(r *httprouter.Router) {
-	r.GET(jamByID, setContentTypeJSON(j.jam))
+	//r.GET(jamByID, setContentTypeJSON(j.jam))
 	r.GET(details, setContentTypeJSON(j.details))
 	r.POST(jamNewR, setContentTypeJSON(j.new))
 	r.POST(joinR, setContentTypeJSON(j.join))
