4d0232f06b91879c083239ab9bf30e11853a4cb3
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-11-21 15:41:35 -0800

    debug

--- utils/utilities.go
+++ utils/utilities.go
@@ -79,7 +79,7 @@ func ParseUpload(r *http.Request) (types.UploadJamParams, error) {
 	jamID := r.FormValue("id")
 	startTime := r.FormValue("start_time")
 	endTime := r.FormValue("end_time")
-	fileName := r.FormValue("file")
+	fileName := r.FormValue("audioFile")
 
 	p := types.UploadJamParams{
 		UserID:      userID,
