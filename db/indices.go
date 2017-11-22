7bc79ba31fff92944b624f8b8b3d6edeca01cabc
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-11-21 17:09:46 -0800

    indices

--- db/indices.go
+++ db/indices.go
@@ -9,6 +9,6 @@ func EnsureIndices() {
 	defer db.Close()
 
 	db.JamCollection().EnsureIndex(mgo.Index{Key: []string{"pin"}, Unique: true, Name: "pin_index"})
-	db.RecordingsCollection().EnsureIndex(mgo.Index{Key: []string{"jam_id", "user_id"}})
+	db.RecordingsCollection().EnsureIndex(mgo.Index{Key: []string{"jam_id", "user_id"}, Unique: false, Name: "user_Jam"})
 	db.UserCollection().EnsureIndex(mgo.Index{Key: []string{"fb_id"}})
 }
