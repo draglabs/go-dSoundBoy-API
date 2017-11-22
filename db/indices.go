dc82203193553e83542a7035718f3d475ae8c9dc
Marlon Monroy <monroy@Marlons-MacBook-Pro.local>
2017-11-21 17:04:43 -0800

    indices

--- db/indices.go
+++ db/indices.go
@@ -7,12 +7,8 @@ import (
 func EnsureIndices() {
 	db := NewDB()
 	defer db.Close()
-	db.RecordingsCollection().EnsureIndex(mgo.Index{
-		Key:    []string{"user_id", "jam_id"},
-		Unique: true,
-	})
+
 	db.JamCollection().EnsureIndex(mgo.Index{Key: []string{"pin"}, Unique: true, Name: "pin_index"})
-	db.RecordingsCollection().EnsureIndex(mgo.Index{Key: []string{"jam_id"}})
 	db.RecordingsCollection().EnsureIndex(mgo.Index{Key: []string{"jam_id", "user_id"}})
 	db.UserCollection().EnsureIndex(mgo.Index{Key: []string{"fb_id"}})
 }
